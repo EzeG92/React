0866788e02e99e64f2e0a07e1fbd1236fa44fe1c
EzeG92 <102333584+EzeG92@users.noreply.github.com>
2022-08-27 20:30:26 -0300

    Barra navegacion"

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,21 @@
+import './NavBar.css';
+
+const NavBar = () => {
+    
+    return (
+    <div className="menu">
+        <nav>
+            <ul>
+                <li><a href="" className='ini'>Inicio </a></li>
+                <li><a href="">Productos</a></li>
+                <li><a href="">Quienes somos</a></li>
+                <li><a href="">Como comprar</a></li>
+                <li><a href="">Contacto</a></li>
+            </ul>
+        </nav>
+    </div>
+    )
+
+}
+
+export default NavBar;
